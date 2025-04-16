cf412ef59c899cb528befde66b694453d6e7f33d
DESKTOP-UQ2F4N6\jayde <jaydee34551@gmail.com>
2025-04-16 23:21:54 +0800

    Test

--- Coffee Ordering System/src/coffee/ordering/system/CoffeeOrderingSystem.java
+++ Coffee Ordering System/src/coffee/ordering/system/CoffeeOrderingSystem.java
@@ -14,7 +14,7 @@ public class CoffeeOrderingSystem {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("hwda");
+        System.out.println("TEST");
     }
     
 }
